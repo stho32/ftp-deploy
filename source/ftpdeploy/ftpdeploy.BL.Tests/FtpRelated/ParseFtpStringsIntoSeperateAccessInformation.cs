72dba87be3a017217452e15afc003b2ee096fc38
stho32 <stho32github@gmail.com>
2021-11-28 08:40:41 +0100

    parsing ftp connection information now works

--- source/ftpdeploy/ftpdeploy.BL.Tests/FtpRelated/ParseFtpStringsIntoSeperateAccessInformation.cs
+++ source/ftpdeploy/ftpdeploy.BL.Tests/FtpRelated/ParseFtpStringsIntoSeperateAccessInformation.cs
@@ -15,24 +15,55 @@ public class ParseFtpStringsIntoSeperateAccessInformationTests {
         Assert.Equal(1234, accessInformation.Port);
     }
 
+    [Fact]
     public void Passwords_with_doublepoint_work() {
-        
+        var testString = "user:pass::word@somehost.com:1234";
+
+        var accessInformation = FtpStringParser.Parse(testString);
+
+        Assert.Equal("user", accessInformation.User);
+        Assert.Equal("pass::word", accessInformation.Password);
+        Assert.Equal("somehost.com", accessInformation.Host);
+        Assert.Equal(1234, accessInformation.Port);
     }
 
+    [Fact]
     public void When_the_port_is_not_given_then_21_is_always_assumed() {
-        
+        var testString = "user:password@somehost.com";
+
+        var accessInformation = FtpStringParser.Parse(testString);
+
+        Assert.Equal("user", accessInformation.User);
+        Assert.Equal("password", accessInformation.Password);
+        Assert.Equal("somehost.com", accessInformation.Host);
+        Assert.Equal(21, accessInformation.Port);
     }
 
+    [Fact]
     public void When_the_host_is_missing_an_exception_is_thrown() {
-        
+        var testString = "user:password";
+
+        Assert.Throws<Exception>(() => {
+            var accessInformation = FtpStringParser.Parse(testString);
+        });
     }
 
+    [Fact]
     public void When_the_user_is_missing_an_exception_is_thrown() {
-        
+        var testString = "password@somehost.com";
+
+        Assert.Throws<Exception>(() => {
+            var accessInformation = FtpStringParser.Parse(testString);
+        });
     }
 
+    [Fact]
     public void When_the_password_is_missing_an_exception_is_thrown() {
-        
+        var testString = "user@somehost.com";
+
+        Assert.Throws<Exception>(() => {
+            var accessInformation = FtpStringParser.Parse(testString);
+        });
     }
 
 }
--- source/ftpdeploy/ftpdeploy.BL/FtpRelated/FtpStringParser.cs
+++ source/ftpdeploy/ftpdeploy.BL/FtpRelated/FtpStringParser.cs
@@ -6,12 +6,27 @@ public static class FtpStringParser {
     public static IFtpAccessInformation Parse(string input) {
         // user:password@somehost.com:1234
         var user = GetUntil(":", ref input);
+        if (string.IsNullOrEmpty(user)) {
+            throw new Exception("The user is missing.");
+        }
+
         // password@somehost.com:1234
         var password = GetUntil("@", ref input);
+        if (string.IsNullOrEmpty(password)) {
+            throw new Exception("The password is missing.");
+        }
+
         // somehost.com:1234
         var host = GetUntil(":", ref input);
+        if (string.IsNullOrEmpty(host)) {
+            throw new Exception("The host is missing.");
+        }
+
         // 1234
-        var port = input.ToInt();
+        var port = 21;
+        if (!string.IsNullOrWhiteSpace(input)) {
+            port = input.ToInt();
+        }
 
         return new FtpAccessInformation(user, password, host, port);
     }
